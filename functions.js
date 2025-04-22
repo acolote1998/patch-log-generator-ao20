456f51a5237617cf7d431cea03b219cac0b8486f
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-04-22 21:52:32 +0200

    getPatchLog makes inputs into clean urls

--- functions.js
+++ functions.js
@@ -7,3 +7,12 @@ function addMoreInputs() {
   inputDivs.appendChild(newBr);
   inputDivs.appendChild(newInput);
 }
+
+async function getPatchLog() {
+  let linkObjects = document.querySelectorAll("input");
+  let linksUrls = [];
+  for (let link of linkObjects) {
+    linksUrls.push(link.value);
+  }
+  console.log(linksUrls);
+}
