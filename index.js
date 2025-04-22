c391dc46e5d915c5af8fc1b4bfe40fa2828c281c
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-04-22 20:30:19 +0200

    improving logic flow

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-function callGitHubApi(pullRequestLink) {
+async function callGitHubApi(pullRequestLink) {
   let modifiedString = pullRequestLink.replace(
     "https://github.com/",
     "https://api.github.com/repos/"
   );
   modifiedString = modifiedString.replace("pull", "pulls");
   let pullRequest;
-  console.log(modifiedString);
-  fetch(modifiedString)
-    .then((response) => response.json())
-    .then((response) => {
-      let prTitle = response.title;
-      let autor = response.user.login;
-      let descripcion = response.body;
-      let prNumber = response.number;
-      pullRequest = {
-        titulo: prTitle,
-        autor: autor,
-        descripcion: descripcion,
-        prNumber: prNumber,
-      };
-      console.log(pullRequest);
-    });
+  const response = await fetch(modifiedString);
+  const data = await response.json();
+  return {
+    titulo: data.title,
+    autor: data.user.login,
+    descripcion: data.body,
+    prNumber: data.number,
+  };
 }
+
+/*
+(async () => {
+  const prData = await callGitHubApi('https://github.com/ao-org/argentum-online-server/pull/759');
+  console.log(prData);
+})();
+*/
