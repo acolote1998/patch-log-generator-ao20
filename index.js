da05ecbd871a6ce07036fa32d62a8643f68da096
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-04-22 21:07:04 +0200

    calling gemini working successfully

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-async function callGitHubApi(pullRequestLink) {
+import geminiKey from "./key.js";
+
+export async function callGitHubApi(pullRequestLink) {
   let modifiedString = pullRequestLink.replace(
     "https://github.com/",
     "https://api.github.com/repos/"
@@ -21,3 +23,31 @@ async function callGitHubApi(pullRequestLink) {
   console.log(prData);
 })();
 */
+
+export async function callGemini(text) {
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${geminiKey}`;
+
+  // The data you want to send in the request body
+  const requestBody = {
+    contents: [
+      {
+        parts: [{ text: `Favorite music of Argentinian musicians` }],
+      },
+    ],
+  };
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", // Set the content type to JSON
+    },
+    body: JSON.stringify(requestBody), // Convert the body to a JSON string
+  });
+
+  const data = await response.json();
+
+  console.log(data.candidates[0].content.parts[0].text);
+}
+
+window.callGitHubApi = callGitHubApi;
+window.callGemini = callGemini;
